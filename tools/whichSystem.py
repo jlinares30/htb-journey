41612225b22b8eac4afe06f3ddc226afa2f3986a
jorgeLinares <jorgelinaresrrs306@hotmail.com>
2026-06-03 16:58:38 -0500

    feat: add whichSystem tool to determine OS based on TTL from ping response

---
+++ tools/whichSystem.py
@@ -0,0 +1,36 @@
+import re, sys, subprocess 
+from termcolor import colored  # type: ignore
+
+
+if len(sys.argv) != 2:
+    print( "\n[!] Uso: python3" + sys.argv[0] + "<direccion-ip>\n")
+    sys.exit(1)
+
+
+def get_ttl(ip_address):
+    proc = subprocess.Popen(["/usr/bin/ping -c 1 %s" % ip_address, ""], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    
+    
+    out = out.split()
+    out = out[12].decode('utf-8')
+
+    ttl_value = re.findall(r'\d{1,3}', out)[0]
+
+    return ttl_value
+
+def get_os(ttl):
+    ttl = int(ttl)
+
+    if ttl >= 0 and ttl <= 64:
+        return "Linux"
+    elif ttl >= 65 and ttl <=128:
+        return "Windows"
+    else:
+        return "Not Found"
+
+if __name__ == "__main__":
+    ip_address = sys.argv[1]
+    ttl = get_ttl(ip_address)
+    os_name = colored(get_os(ttl), 'blue')
+    print(colored("\n[+] %s (ttl => %s): %s\n" % (ip_address, ttl, os_name), 'yellow'))
